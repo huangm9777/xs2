ae7a63ff263ea889235e932e109345f2f857a133
Mingfu.H <huangm9777@uwec.edu>
2024-09-12 15:26:11 -0700

    init

---
+++ xs2-test/src/main/java/com/bdk/startertest/StarterTest.java
@@ -0,0 +1,12 @@
+package com.bdk.startertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterTest {
+    public static void main(String[] args) {
+        SpringApplication.run(StarterTest.class,args);
+
+    }
+}
